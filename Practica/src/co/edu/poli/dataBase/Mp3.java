7374036fe75d866e9bcaae00a1ff640fe105f14f
nicoj <nicoj@PIPI>
2025-10-20 01:36:27 -0500

    correcion

--- Practica/src/co/edu/poli/dataBase/Mp3.java
+++ Practica/src/co/edu/poli/dataBase/Mp3.java
@@ -3,18 +3,21 @@ package co.edu.poli.dataBase;
 import java.util.List;
 
 public class Mp3 {
+
     private int idMp3;
     private String formato;
     private double tamanioMB;
+    private double precio;
     private Cancion cancion;
 
     public Mp3() {
     }
 
-    public Mp3(int idMp3, String formato, double tamanioMB, Cancion cancion) {
+    public Mp3(int idMp3, String formato, double tamanioMB, double precio, Cancion cancion) {
         this.idMp3 = idMp3;
         this.formato = formato;
         this.tamanioMB = tamanioMB;
+        this.precio = precio;
         this.cancion = cancion;
     }
 
@@ -42,6 +45,14 @@ public class Mp3 {
         this.tamanioMB = tamanioMB;
     }
 
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
     public Cancion getCancion() {
         return cancion;
     }
@@ -52,19 +63,23 @@ public class Mp3 {
 
     @Override
     public String toString() {
-        return "Mp3 [idMp3=" + idMp3 + ", formato=" + formato + ", tamanioMB=" + tamanioMB + ", cancion=" + cancion.getTitulo() + "]";
+        return "Mp3 [idMp3=" + idMp3 + ", formato=" + formato + ", tamanioMB=" + tamanioMB +
+               " MB, precio=$" + precio + ", cancion=" + cancion.getTitulo() + "]";
     }
 
+    // 🔹 Lista de ejemplos con precios agregados
     public static final List<Mp3> Mp3 = List.of(
-        new Mp3(1, "mp3", 5.2, new Cancion(1, "Blinding Lights", "The Weeknd", 3.2)),
-        new Mp3(2, "mp3", 4.8, new Cancion(2, "As It Was", "Harry Styles", 2.8)),
-        new Mp3(3, "mp3", 6.0, new Cancion(3, "Levitating", "Dua Lipa", 3.4)),
-        new Mp3(4, "mp3", 5.0, new Cancion(4, "Bad Guy", "Billie Eilish", 3.1)),
-        new Mp3(5, "mp3", 6.5, new Cancion(5, "Shape of You", "Ed Sheeran", 4.0))
+        new Mp3(1, "mp3", 5.2, 4500, new Cancion(1, "Blinding Lights", "The Weeknd", 3.2)),
+        new Mp3(2, "mp3", 4.8, 4200, new Cancion(2, "As It Was", "Harry Styles", 2.8)),
+        new Mp3(3, "mp3", 6.0, 5000, new Cancion(3, "Levitating", "Dua Lipa", 3.4)),
+        new Mp3(4, "mp3", 5.0, 4700, new Cancion(4, "Bad Guy", "Billie Eilish", 3.1)),
+        new Mp3(5, "mp3", 6.5, 5200, new Cancion(5, "Shape of You", "Ed Sheeran", 4.0))
     );
 
-	public void setTitulo(String nuevoTitulo) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void setTitulo(String nuevoTitulo) {
+        if (this.cancion != null) {
+            this.cancion.setTitulo(nuevoTitulo);
+        }
+    }
 }
+
